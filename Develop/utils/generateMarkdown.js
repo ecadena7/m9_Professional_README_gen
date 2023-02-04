58f6791f584a0a2e528fc0fffc9fd02fe4425090
Eli Cadena <103008210+ecadena7@users.noreply.github.com>
2023-02-04 16:15:51 -0700

    updated functions and linked them to each section

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -11,7 +11,7 @@ return ''
 // If there is no license, return an empty string
 function renderLicenseLink(license) {
   if (license !== 'None') {
-    return ''
+    return '[${data.license}](https://github.com/ecadena7/m9_Professional_README_gen/license)'
   }
   return ''
 }
@@ -20,7 +20,7 @@ function renderLicenseLink(license) {
 // If there is no license, return an empty string
 function renderLicenseSection(license) {
   if (license !== 'None') {
-    return ``;
+    return '${data.license} License.';
   }
   return '';
 }
@@ -28,7 +28,11 @@ function renderLicenseSection(license) {
 // Created a function that returns Credits
 function renderCreditsSection(credit) {
   if (credit !== 'None') {
-    return ``;
+    return `## Credits
+  
+    The following tutorials were used for this README.md file:
+      -[How to create a Professional README](https://coding-boot-camp.github.io/full-stack/github/professional-readme-guide)
+      -[How to add a video into your README](https://coding-boot-camp.github.io/full-stack/computer-literacy/video-submission-guide)`;
   }
   return '';
 }
@@ -36,7 +40,10 @@ function renderCreditsSection(credit) {
 //created a function that returns Collaborators
 function renderCollaboratorSection(collaborators) {
   if (collaborators !== 'None') {
-    return ``;
+    return `  The collaborators that worked on this project were:
+    - [Starter Code](https://github.com/coding-boot-camp/potential-enigma)
+    - ${data.collaborators}
+    `;
   }
   return '';
 }
@@ -46,72 +53,83 @@ function generateMarkdown(data) {
   return `# ${data.title}
 
   ## Description
+
+  ${data.description}
+  `
+//   Provide a short description explaining the what, why, and how of your project. Use the following questions as a guide:
   
-  Provide a short description explaining the what, why, and how of your project. Use the following questions as a guide:
+//   - What was your motivation?
+//   - Why did you build this project? (Note: the answer is not "Because it was a homework assignment.")
+//   - What problem does it solve?
+//   - What did you learn?
   
-  - What was your motivation?
-  - Why did you build this project? (Note: the answer is not "Because it was a homework assignment.")
-  - What problem does it solve?
-  - What did you learn?
+//   ## Table of Contents (Optional)
   
-  ## Table of Contents (Optional)
+//   If your README is long, add a table of contents to make it easy for users to find what they need.
   
-  If your README is long, add a table of contents to make it easy for users to find what they need.
-  
-  - [Installation](#installation)
-  - [Usage](#usage)
-  - [Credits](#credits)
-  - [License](#license)
-  
+//   - [Installation](#installation)
+//   - [Usage](#usage)
+//   - [Credits](#credits)
+//   - [License](#license)
+  `  
   ## Installation
   
-  What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.
-  
+  `//What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.`
+  `
+  ${data.installation}
+
   ## Usage
+
+  ${data.usage}
+  `
+//   Provide instructions and examples for use. Include screenshots as needed.
   
-  Provide instructions and examples for use. Include screenshots as needed.
+//   To add a screenshot, create an `assets/images` folder in your repository and upload your screenshot to it. Then, using the relative filepath, add it to your README using the following syntax:
   
-  To add a screenshot, create an `assets/images` folder in your repository and upload your screenshot to it. Then, using the relative filepath, add it to your README using the following syntax:
+//       ```md
+//       ![alt text](assets/images/screenshot.png)
+//       ```
+  `
+  ${data.credit}
+
+  ${data.collaborators}
+  `
+//   List your collaborators, if any, with links to their GitHub profiles.
+
+//   If you used any third-party assets that require attribution, list the creators with links to their primary web presence in this section.
   
-      ```md
-      ![alt text](assets/images/screenshot.png)
-      ```
-  
-  ## Credits
-  
-  List your collaborators, if any, with links to their GitHub profiles.
-  
-  If you used any third-party assets that require attribution, list the creators with links to their primary web presence in this section.
-  
-  If you followed tutorials, include links to those here as well.
-  
+//   If you followed tutorials, include links to those here as well.
+  `
+
   ## License
   
-  The last section of a high-quality README file is the license. This lets other developers know what they can and cannot do with your project. If you need help choosing a license, refer to [https://choosealicense.com/](https://choosealicense.com/).
+  ${data.license}
+  `
+  //  The last section of a high-quality README file is the license. This lets other developers know what they can and cannot do with your project. If you need help choosing a license, refer to [https://choosealicense.com/](https://choosealicense.com/).
+  `  
+  `
+//   ---
   
-  ---
+//   🏆 The previous sections are the bare minimum, and your project will ultimately determine the content of this document. You might also want to consider adding the following sections.
   
-  🏆 The previous sections are the bare minimum, and your project will ultimately determine the content of this document. You might also want to consider adding the following sections.
+//   ## Badges
   
-  ## Badges
+//   ![badmath](https://img.shields.io/github/languages/top/lernantino/badmath)
   
-  ![badmath](https://img.shields.io/github/languages/top/lernantino/badmath)
+//   Badges aren't necessary, per se, but they demonstrate street cred. Badges let other developers know that you know what you're doing. Check out the badges hosted by [shields.io](https://shields.io/). You may not understand what they all represent now, but you will in time.
   
-  Badges aren't necessary, per se, but they demonstrate street cred. Badges let other developers know that you know what you're doing. Check out the badges hosted by [shields.io](https://shields.io/). You may not understand what they all represent now, but you will in time.
+//   ## Features
   
-  ## Features
+//   If your project has a lot of features, list them here.
   
-  If your project has a lot of features, list them here.
+//   ## How to Contribute
   
-  ## How to Contribute
-  
-  If you created an application or package and would like other developers to contribute it, you can include guidelines for how to do so. The [Contributor Covenant](https://www.contributor-covenant.org/) is an industry standard, but you can always write your own if you'd prefer.
-  
+//   If you created an application or package and would like other developers to contribute it, you can include guidelines for how to do so. The [Contributor Covenant](https://www.contributor-covenant.org/) is an industry standard, but you can always write your own if you'd prefer.
+  `
   ## Tests
   
+  ${data.test}
   Go the extra mile and write tests for your application. Then provide examples on how to run them here.
-
-  
 `;
 }
 
