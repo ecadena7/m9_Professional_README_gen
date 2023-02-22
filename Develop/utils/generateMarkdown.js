654c55757f7bb0386d6d8f19fad74866c04ad511
Eli Cadena <103008210+ecadena7@users.noreply.github.com>
2023-02-22 11:52:24 -0700

    updated LicenseLink, and Badge functions

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -1,23 +1,41 @@
 // TODO: Create a function that returns a license badge based on which license is passed in
+
+const { default: inquirer } = require("inquirer");
+
 // If there is no license, return an empty string
 function renderLicenseBadge(license) {
-  
+  if (license !== 'None') {
+    return `![](https://img.shields.io/badge/license-${license}-orange)`
+  };
 }
 
 // TODO: Create a function that returns the license link
 // If there is no license, return an empty string
-function renderLicenseLink(license) {}
+function renderLicenseLink(license) {
+  if (license === 'MIT') {
+    return '[MIT Link](https://mit-license.org/)'
+  };
+  if (license === 'APACHE') {
+    return '[APACHE Link]()'
+  };
+  if (license === 'GNU') {
+    return '[GNU Link]()'
+  }
+};
 
 // TODO: Create a function that returns the license section of README
 // If there is no license, return an empty string
-function renderLicenseSection(license) {}
+function renderLicenseSection(license) {
+  return `
+  # ${license}
+  
+  Did this also print on the screen?`
+}
 
 // TODO: Create a function to generate markdown for README
 function generateMarkdown(data) {
   return `# ${data.title}
 
-  ![badmath](https://img.shields.io/github/languages/top/lernantino/badmath)
-  
 ## Description
 
 ${data.description}
@@ -37,6 +55,7 @@ If your README is long, add a table of contents to make it easy for users to fin
 - [Usage](#usage)
 - [Credits](#credits)
 - [License](#license)
+- [Contact](#contact)
 
 ## Installation
 
@@ -53,6 +72,7 @@ ${data.usage}
 ## Credits
 
 List your collaborators, if any, with links to their GitHub profiles.
+    - [GitHub Profile:](https://github.com/${data.githubTwo})
 
 If you used any third-party assets that require attribution, list the creators with links to their primary web presence in this section.
 
@@ -60,7 +80,11 @@ If you followed tutorials, include links to those here as well.
 
 ## License
 
-${data.license}
+${data.license} 
+
+${renderLicenseBadge(data.license)}
+
+${renderLicenseLink(data.license)}
 
 The last section of a high-quality README file is the license. This lets other developers know what they can and cannot do with your project. If you need help choosing a license, refer to [https://choosealicense.com/](https://choosealicense.com/).
 
@@ -68,19 +92,15 @@ The last section of a high-quality README file is the license. This lets other d
 
 🏆 The previous sections are the bare minimum, and your project will ultimately determine the content of this document. You might also want to consider adding the following sections.
 
-## Badges
+## Contact
 
-Badges aren't necessary, per se, but they demonstrate street cred. Badges let other developers know that you know what you're doing. Check out the badges hosted by [shields.io](https://shields.io/). You may not understand what they all represent now, but you will in time.
+Github: [https://github.com/${data.github}](https://github.com/${data.github})
 
-## Questions
-
-[GitHub Profile:](https://github.com/${data.github})
-
-[Email:](${data.email})
+Email: [${data.email}](mailto:${data.email})
 
 ## How to Contribute
 
-${data.contributing}
+${data.contribution}
 If you created an application or package and would like other developers to contribute it, you can include guidelines for how to do so. The [Contributor Covenant](https://www.contributor-covenant.org/) is an industry standard, but you can always write your own if you'd prefer.
 
 ## Tests
