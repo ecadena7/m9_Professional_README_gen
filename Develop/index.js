9df8ceb2b754b2d03cda6b3a260682fa29fe2557
Eli Cadena <103008210+ecadena7@users.noreply.github.com>
2023-02-22 11:53:59 -0700

    updated initialize app funciton. removed the [...] next to inquirerResponses

--- Develop/index.js
+++ Develop/index.js
@@ -8,49 +8,49 @@ const generateMarkdown = require('./utils/generateMarkdown');
 
 // TODO: Create an array of questions for user input
 const questions = [
-    // {
-    //     type: 'input',
-    //     name: 'title',
-    //     message: 'What is your projects name?'
-    // },
-    // {
-    //     type: 'input',
-    //     name: 'description',
-    //     message: 'Please write a short description of you project.',
-    // },
-    // {
-    //     type: 'input',
-    //     name: 'installation',
-    //     message: 'What command should be run to install dependencies?',
-    //     default: 'npm i',
-    // },
-    // {
-    //     type: 'input',
-    //     name: 'usage',
-    //     message: 'What does the user need to know about using the repo?'
-    // },
-    // {
-    //     type: 'input',
-    //     name: 'collaborators',
-    //     message: 'Were there any collaborators?'
-    // },
-    // {
-    //     type: 'list',
-    //     name: 'license',
-    //     message: 'Choose the type of license for this project?',
-    //     choices: ['MIT', 'None'],
-    // },
-    // {
-    //     type: 'input',
-    //     name: 'test',
-    //     message: 'What command should be run to run tests?',
-    //     default: 'npm test',
-    // },
-    // {
-    //     type: 'input',
-    //     name: 'contributing',
-    //     message: 'What does the user need to know about contributing to the repo?'
-    // },
+    {
+        type: 'input',
+        name: 'title',
+        message: 'What is your projects name?'
+    },
+    {
+        type: 'input',
+        name: 'description',
+        message: 'Please write a short description of you project.',
+    },
+    {
+        type: 'input',
+        name: 'installation',
+        message: 'What command should be run to install dependencies?',
+        default: 'npm i',
+    },
+    {
+        type: 'input',
+        name: 'usage',
+        message: 'What does the user need to know about using the repo?'
+    },
+    {
+        type: 'input',
+        name: 'githubTwo',
+        message: 'What is the github username of a collaborator?'
+    },
+    {
+        type: 'list',
+        name: 'license',
+        message: 'Choose the type of license for this project?',
+        choices: ['MIT', 'APACHE', 'GNU', 'None'],
+    },
+    {
+        type: 'input',
+        name: 'test',
+        message: 'What command should be run to run tests?',
+        default: 'npm test',
+    },
+    {
+        type: 'input',
+        name: 'contribution',
+        message: 'How can you contribute to the repo?'
+    },
     {
         type: 'input',
         name: 'github',
@@ -71,7 +71,7 @@ function writeToFile(fileName, data) {
 // TODO: Create a function to initialize app
 function init() {
     inquirer.prompt(questions).then((inquirerResponses) => {
-        writeToFile('READ.md', generateMarkdown({ ...inquirerResponses}));
+        writeToFile('READ.md', generateMarkdown(inquirerResponses));
     });
 }
 
