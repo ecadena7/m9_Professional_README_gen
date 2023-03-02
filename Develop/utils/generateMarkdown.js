1df6a6690018931b0047b4cf866b4beeccaf20ee
Eli Cadena <103008210+ecadena7@users.noreply.github.com>
2023-03-02 14:03:24 -0700

    cleaned up the text found in the generateMarkdown.js file.

--- Develop/utils/generateMarkdown.js
+++ Develop/utils/generateMarkdown.js
@@ -37,16 +37,7 @@ function generateMarkdown(data) {
 
 ${data.description}
 
-Provide a short description explaining the what, why, and how of your project. Use the following questions as a guide:
-
-- What was your motivation?
-- Why did you build this project? (Note: the answer is not "Because it was a homework assignment.")
-- What problem does it solve?
-- What did you learn?
-
-## Table of Contents (Optional)
-
-If your README is long, add a table of contents to make it easy for users to find what they need.
+## Table of Contents
 
 - [Installation](#installation)
 - [Usage](#usage)
@@ -56,24 +47,26 @@ If your README is long, add a table of contents to make it easy for users to fin
 
 ## Installation
 
-What are the steps required to install your project? Provide a step-by-step description of how to get the development environment running.
+What are the steps required to install your project? 
 
 ${data.installation}
 
 ## Usage
 
-Provide instructions and examples for use. Include screenshots as needed.
-
 ${data.usage}
 
 ## Credits
 
-List your collaborators, if any, with links to their GitHub profiles.
-    - [GitHub Profile:](https://github.com/${data.githubTwo})
+Collaborators,
+    - Github: [${data.githubTwo}](https://github.com/${data.githubTwo})
 
-If you used any third-party assets that require attribution, list the creators with links to their primary web presence in this section.
 
-If you followed tutorials, include links to those here as well.
+Tutorials,
+    - [How to create a Professional README](https://coding-boot-camp.github.io/full-stack/github/professional-readme-guide)
+    - [Microsoft's VS Code repository](https://github.com/microsoft/vscode)
+    - [Contributor Covenant](https://www.contributor-covenant.org/)
+    - [shields.io](https://shields.io/)
+    - [https://choosealicense.com/](https://choosealicense.com/)
 
 ## License
 
@@ -83,22 +76,16 @@ ${renderLicenseBadge(data.license)}
 
 ${renderLicenseLink(data.license)}
 
-The last section of a high-quality README file is the license. This lets other developers know what they can and cannot do with your project. If you need help choosing a license, refer to [https://choosealicense.com/](https://choosealicense.com/).
-
----
-
-🏆 The previous sections are the bare minimum, and your project will ultimately determine the content of this document. You might also want to consider adding the following sections.
-
 ## Contact Me
 
-Github: [https://github.com/${data.github}](https://github.com/${data.github})
+Github: [${data.github}](https://github.com/${data.github})
 
 Email: [${data.email}](mailto:${data.email})
 
 ## How to Contribute
 
 ${data.contribution}
-If you created an application or package and would like other developers to contribute it, you can include guidelines for how to do so. The [Contributor Covenant](https://www.contributor-covenant.org/) is an industry standard, but you can always write your own if you'd prefer.
+If you created an application or package and would like other developers to  is an industry standard, but you can always write your own if you'd prefer.
 
 ## Tests
   
